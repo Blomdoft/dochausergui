bbe9daf455dddd30f0b080d0dc438dcae25e89f2
Florian Wientzek <florian.wientzek@gmx.net>
2022-03-16 16:54:04 +0100

    Updated pathes

--- src/app/ui/document-list/document-panel/document-panel.component.ts
+++ src/app/ui/document-list/document-panel/document-panel.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {PDFDocument} from "../../../model/document.model";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-document-panel',
@@ -11,7 +12,7 @@ export class DocumentPanelComponent implements OnInit {
   @Input() document: PDFDocument;
   currentPreviewPage: number = 0;
 
-  constructor() {
+  constructor(private location: Location) {
     // proper init of empty document makes this safer
     this.document = {
       id: "", name: "", directory: "", text: "", timestamp: "", origin: "",   thumbnails: [],
@@ -35,27 +36,21 @@ export class DocumentPanelComponent implements OnInit {
   }
 
   getPreviewPageURL() : string {
-    return this.document.thumbnails[this.currentPreviewPage].imgdirectory+this.document.thumbnails[this.currentPreviewPage].imgname;
+    return location.origin+"/"+this.document.thumbnails[this.currentPreviewPage].imgdirectory+this.document.thumbnails[this.currentPreviewPage].imgname;
   }
 
   getFullDocumentURL() : string {
-    return this.document.directory + this.document.name;
+    return location.origin+"/"+this.document.directory + this.document.name;
   }
 
   getDisplayDate() : string {
-
     let year: string = this.document.timestamp.substring(0, 4);
     let month: string = this.document.timestamp.substring(4, 6);
     let day: string = this.document.timestamp.substring(6, 8);
-
     let hour: string = this.document.timestamp.substring(9, 11);
     let minute: string = this.document.timestamp.substring(11, 13);
-
     return day + "-"+month+"-"+year + " " +hour+":"+minute;
-
  }
-
-
 
 
 }
